d5370123d52f9bcf5d0f6b3f8db8e2bfd298c454
akshatsinghania <singhaniaakshat1@gmail.com>
2024-07-21 19:37:22 +0530

    partially added function and style to show the sequence of turn

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ const selectBox = document.querySelector(".select-box");
 selectXBtn = selectBox.querySelector(".playerX");
 selectOBtn = selectBox.querySelector(".playerO");
 playBoard = document.querySelector(".play-board");
+allBox = document.querySelectorAll("section span");
+players = document.querySelectorAll(".players");
 
 window.onload = () => {
   //once window loaded
+  for (let i = 0; i < allBox.length; i++) {
+    // add onclick attribute in all available section spans
+    allBox[i].setAttribute("onclick", "clickedBox(this)");
+  }
+
   selectXBtn.onclick = () => {
     selectBox.classList.add("hide"); //hide the select box on playerX button clicked
     playBoard.classList.add("show"); //show the playboard section  on playerX button clicked
@@ -13,5 +20,12 @@ window.onload = () => {
   selectOBtn.onclick = () => {
     selectBox.classList.add("hide"); //hide the select box on playerO button clicked
     playBoard.classList.add("show"); //show the playboard section  on playerO button clicked
+    players.setAttribute("class", "players active");
   };
 };
+let playerXIcon = "fas fa-times"; //classname of fontawesome cross icon
+let playerOIcon = "far fa-circle"; //classname of fontawesome circle
+
+function clickedBox(element) {
+  // if(){}
+}
