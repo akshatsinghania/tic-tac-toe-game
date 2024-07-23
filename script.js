3f57bb689f211c249295638024badd4942912369
akshatsinghania <singhaniaakshat1@gmail.com>
2024-07-23 20:25:46 +0530

    added reload function and completed tic tac toe game

--- script.js
+++ script.js
@@ -1,83 +1,137 @@
-//selecting all required elements
-const selectBox = document.querySelector(".select-box");
-selectXBtn = selectBox.querySelector(".playerX");
-selectOBtn = selectBox.querySelector(".playerO");
-playBoard = document.querySelector(".play-board");
-allBox = document.querySelectorAll("section span");
-players = document.querySelector(".players");
+const selectBox = document.querySelector(".select-box"),
+  selectBtnX = selectBox.querySelector(".options .playerX"),
+  selectBtnO = selectBox.querySelector(".options .playerO"),
+  playBoard = document.querySelector(".play-board"),
+  players = document.querySelector(".players"),
+  allBox = document.querySelectorAll("section span"),
+  resultBox = document.querySelector(".result-box"),
+  wonText = resultBox.querySelector(".won-text"),
+  replayBtn = resultBox.querySelector("button");
 
 window.onload = () => {
-  //once window loaded
   for (let i = 0; i < allBox.length; i++) {
-    // add onclick attribute in all available section spans
     allBox[i].setAttribute("onclick", "clickedBox(this)");
   }
-
-  selectXBtn.onclick = () => {
-    selectBox.classList.add("hide"); //hide the select box on playerX button clicked
-    playBoard.classList.add("show"); //show the playboard section  on playerX button clicked
-  };
-  selectOBtn.onclick = () => {
-    selectBox.classList.add("hide"); //hide the select box on playerO button clicked
-    playBoard.classList.add("show"); //show the playboard section  on playerO button clicked
-    players.setAttribute("class", "players active player"); //adding three classname in player element
-  };
 };
-let playerXIcon = "fas fa-times"; //classname of fontawesome cross icon
-let playerOIcon = "far fa-circle"; //classname of fontawesome circle icon
-let playerSign = "X"; //suppose player will be X
 
-//user clicked function
+selectBtnX.onclick = () => {
+  selectBox.classList.add("hide");
+  playBoard.classList.add("show");
+};
+
+selectBtnO.onclick = () => {
+  selectBox.classList.add("hide");
+  playBoard.classList.add("show");
+  players.setAttribute("class", "players active player");
+};
+
+let playerXIcon = "fas fa-times",
+  playerOIcon = "far fa-circle",
+  playerSign = "X",
+  runBot = true;
+
 function clickedBox(element) {
-  //console.log(element);
   if (players.classList.contains("player")) {
-    //if players element has contains .player
-
+    playerSign = "O";
     element.innerHTML = `<i class="${playerOIcon}"></i>`;
-    players.classList.add("active");
-    playerSign = "0";
+    players.classList.remove("active");
     element.setAttribute("id", playerSign);
   } else {
-    element.innerHTML = `<i class="${playerXIcon}"></i>`; //adding circle icon tag inside user clicked elemen
-    players.classList.add("active");
+    element.innerHTML = `<i class="${playerXIcon}"></i>`;
     element.setAttribute("id", playerSign);
+    players.classList.add("active");
   }
-  element.style.pointerEvents = "none"; //once user select any box then that box cant be selected again
-  let randomDelayTime = (Math.random() * 1000 + 200).toFixed(); //generating randomly time delay to select box
-
+  selectWinner();
+  element.style.pointerEvents = "none";
+  playBoard.style.pointerEvents = "none";
+  let randomTimeDelay = (Math.random() * 1000 + 200).toFixed();
   setTimeout(() => {
-    bot(); // calling bot function
-  }, randomDelayTime);
+    bot(runBot);
+  }, randomTimeDelay);
 }
 
-//bot click function
 function bot() {
-  playerSign = "0";
   let array = [];
-  for (let i = 0; i < allBox.length; i++) {
-    if (allBox[i].childElementCount == 0) {
-      array.push(i);
-      console.log(i + " " + "has no children");
+  if (runBot) {
+    playerSign = "O";
+    for (let i = 0; i < allBox.length; i++) {
+      if (allBox[i].childElementCount == 0) {
+        array.push(i);
+      }
     }
+    let randomBox = array[Math.floor(Math.random() * array.length)];
+    if (array.length > 0) {
+      if (players.classList.contains("player")) {
+        playerSign = "X";
+        allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`;
+        allBox[randomBox].setAttribute("id", playerSign);
+        players.classList.add("active");
+      } else {
+        allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`;
+        players.classList.remove("active");
+        allBox[randomBox].setAttribute("id", playerSign);
+      }
+      selectWinner();
+    }
+    allBox[randomBox].style.pointerEvents = "none";
+    playBoard.style.pointerEvents = "auto";
+    playerSign = "X";
   }
-  let randomBox = array[Math.floor(Math.random() * array.length)]; //getting random index from array so bot will select
+}
 
-  if (array.length > 0) {
-    if (players.classList.contains("player")) {
-      //if players element has contains .player
-      allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`;
-      players.classList.remove("active");
-      playerSign = "X";
-      allBox[randomBox].setAttribute("id", playerSign);
-    } else {
-      allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`; //adding circle icon tag inside user clicked elemen
-      players.classList.remove("active");
-      allBox[randomBox].setAttribute("id", playerSign);
+function getIdVal(classname) {
+  return document.querySelector(".box" + classname).id;
+}
+function checkIdSign(val1, val2, val3, sign) {
+  if (
+    getIdVal(val1) == sign &&
+    getIdVal(val2) == sign &&
+    getIdVal(val3) == sign
+  ) {
+    return true;
+  }
+}
+function selectWinner() {
+  if (
+    checkIdSign(1, 2, 3, playerSign) ||
+    checkIdSign(4, 5, 6, playerSign) ||
+    checkIdSign(7, 8, 9, playerSign) ||
+    checkIdSign(1, 4, 7, playerSign) ||
+    checkIdSign(2, 5, 8, playerSign) ||
+    checkIdSign(3, 6, 9, playerSign) ||
+    checkIdSign(1, 5, 9, playerSign) ||
+    checkIdSign(3, 5, 7, playerSign)
+  ) {
+    runBot = false;
+    bot(runBot);
+    setTimeout(() => {
+      resultBox.classList.add("show");
+      playBoard.classList.remove("show");
+    }, 700);
+    wonText.innerHTML = `Player <p>${playerSign}</p> won the game!`;
+  } else {
+    if (
+      getIdVal(1) != "" &&
+      getIdVal(2) != "" &&
+      getIdVal(3) != "" &&
+      getIdVal(4) != "" &&
+      getIdVal(5) != "" &&
+      getIdVal(6) != "" &&
+      getIdVal(7) != "" &&
+      getIdVal(8) != "" &&
+      getIdVal(9) != ""
+    ) {
+      runBot = false;
+      bot(runBot);
+      setTimeout(() => {
+        resultBox.classList.add("show");
+        playBoard.classList.remove("show");
+      }, 700);
+      wonText.textContent = "Match has been drawn!";
     }
   }
-  allBox[randomBox].style.pointerEvents = "none"; //once bot  select a box  than user cant select or click that box
 }
-//let work on select the winner
-function getId(){
-  return document.querySelector
-}
+
+replayBtn.onclick = () => {
+  window.location.reload();
+};
